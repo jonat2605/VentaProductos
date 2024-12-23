687783d6652871c57c6b5f8e8cb9e22e675222c8
Jonathan Ariza <jarizman@emeal.nttdata.com>
2024-12-23 16:47:10 -0500

    Primer Commit: se crea la estructura base del proyecto, se agrega el archivo de dependencias del proyecto con gradle

---
+++ src/main/java/com/example/ventaProductos/VentaProductosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ventaProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ventaProductos/VentaProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ventaProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
